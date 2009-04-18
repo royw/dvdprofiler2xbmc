eec2b82bfb9875655196027cbf17a3afbf9db8ec
Roy Wright <roy@wright.org>
2009-04-18 01:10:35 -0500

    added unicode url spec and rewrote to reflect change in API from create TmdbImage

--- spec/fanart_controller_spec.rb
+++ spec/fanart_controller_spec.rb
@@ -1,6 +1,7 @@
 require File.dirname(__FILE__) + '/spec_helper.rb'
 
 require 'tempfile'
+require 'ruby-debug'
 
 describe "FanartController" do
 
@@ -20,6 +21,11 @@ describe "FanartController" do
     Dir.glob(File.join(TMPDIR,'*-fanart*')).each { |filename| File.delete(filename) }
     Dir.glob(File.join(TMPDIR,'*.dummy')).each { |filename| File.delete(filename) }
     Dir.glob(File.join(TMPDIR,'*.tmdb.xml')).each { |filename| File.delete(filename) }
+
+    FileUtils.touch(File.join(TMPDIR, 'Die Hard - 1988.dummy'))
+    media = Media.new(TMPDIR, 'Die Hard - 1988.dummy')
+    media.imdb_id = 'tt0095016'
+    @controller = FanartController.new(media)
   end
 
   after(:all) do
@@ -29,92 +35,73 @@ describe "FanartController" do
   end
 
   it 'should generate "original.0" size destination filespec' do
-    fanart = {'size' => 'original', 'content' => 'http://example.com/foobar.jpg'}
     indexes = {}
-    filespec = FanartController.get_destination_filespec('/tmp/movie title.m4v', fanart, indexes)
-    filespec.should == "/tmp/movie title-fanart.original.0.jpg"
+    filespec = @controller.send('get_destination_filespec', '/tmp/movie title.m4v', 'original', indexes)
+    filespec.should == "/tmp/movie title-fanart.original.0"
   end
+
   it 'should generate "mid.0" size destination filespec' do
-    fanart = {'size' => 'mid', 'content' => 'http://example.com/foobar.jpg'}
     indexes = {}
-    filespec = FanartController.get_destination_filespec('/tmp/movie title.m4v', fanart, indexes)
-    filespec.should == "/tmp/movie title-fanart.mid.0.jpg"
+    filespec = @controller.send('get_destination_filespec', '/tmp/movie title.m4v', 'mid', indexes)
+    filespec.should == "/tmp/movie title-fanart.mid.0"
   end
+
   it 'should generate "thumb.0" size destination filespec' do
-    fanart = {'size' => 'thumb', 'content' => 'http://example.com/foobar.jpg'}
     indexes = {}
-    filespec = FanartController.get_destination_filespec('/tmp/movie title.m4v', fanart, indexes)
-    filespec.should == "/tmp/movie title-fanart.thumb.0.jpg"
+    filespec = @controller.send('get_destination_filespec', '/tmp/movie title.m4v', 'thumb', indexes)
+    filespec.should == "/tmp/movie title-fanart.thumb.0"
   end
+
   it 'should generate "cover.0" size destination filespec' do
-    fanart = {'size' => 'cover', 'content' => 'http://example.com/foobar.jpg'}
     indexes = {}
-    filespec = FanartController.get_destination_filespec('/tmp/movie title.m4v', fanart, indexes)
-    filespec.should == "/tmp/movie title-fanart.cover.0.jpg"
+    filespec = @controller.send('get_destination_filespec', '/tmp/movie title.m4v', 'cover', indexes)
+    filespec.should == "/tmp/movie title-fanart.cover.0"
   end
 
   it 'should generate "original.1" size destination filespec' do
-    fanart = {'size' => 'original', 'content' => 'http://example.com/foobar.jpg'}
     indexes = {'original' => 0}
-    filespec = FanartController.get_destination_filespec('/tmp/movie title.m4v', fanart, indexes)
-    filespec.should == "/tmp/movie title-fanart.original.1.jpg"
+    filespec = @controller.send('get_destination_filespec', '/tmp/movie title.m4v', 'original', indexes)
+    filespec.should == "/tmp/movie title-fanart.original.1"
   end
 
   it 'should increment indexes' do
-    fanart = {'size' => 'cover', 'content' => 'http://example.com/foobar.jpg'}
     indexes = {}
-    filespec = FanartController.get_destination_filespec('/tmp/movie title.m4v', fanart, indexes)  # indexex['cover'] => 0
-    filespec = FanartController.get_destination_filespec('/tmp/movie title.m4v', fanart, indexes)  # indexex['cover'] => 1
-    filespec = FanartController.get_destination_filespec('/tmp/movie title.m4v', fanart, indexes)  # indexex['cover'] => 2
-    indexes['cover'].should == 2 && filespec.should == "/tmp/movie title-fanart.cover.2.jpg"
+    filespec = @controller.send('get_destination_filespec', '/tmp/movie title.m4v', 'original', indexes)  # indexex['original'] => 0
+    filespec = @controller.send('get_destination_filespec', '/tmp/movie title.m4v', 'original', indexes)  # indexex['original'] => 1
+    filespec = @controller.send('get_destination_filespec', '/tmp/movie title.m4v', 'original', indexes)  # indexex['original'] => 2
+    indexes['original'].should == 2 && filespec.should == "/tmp/movie title-fanart.original.2"
   end
 
   it 'should link to a fanart file' do
     dest_filespec = "#{TMPDIR}/foo-fanart"
     link_filespec = "#{dest_filespec}.jpg"
     touch("#{dest_filespec}.original.0.jpg")
-    controller = FanartController.new(nil)
-    controller.link_fanart(dest_filespec)
+    @controller.send('link_fanart', dest_filespec)
     File.exist?(link_filespec).should be_true
   end
 
   it 'should link to the first, largest (original) fanart file' do
     dest_filespec = "#{TMPDIR}/foo-fanart"
     link_filespec = "#{dest_filespec}.jpg"
-    ['original', 'mid', 'cover', 'thumb'].each do |size|
+    ['original', 'mid', 'thumb'].each do |size|
       0.upto(2) do |index|
         touch("#{dest_filespec}.#{size}.#{index}.jpg")
       end
     end
-    controller = FanartController.new(nil)
-    controller.link_fanart(dest_filespec)
+    @controller.send('link_fanart', dest_filespec)
     (File.exist?(link_filespec).should be_true) && (open(link_filespec).read.should == "#{dest_filespec}.original.0.jpg\n")
   end
 
   it 'should link to the first, largest (mid) fanart file' do
     dest_filespec = "#{TMPDIR}/foo-fanart"
     link_filespec = "#{dest_filespec}.jpg"
-    ['mid', 'cover', 'thumb'].each do |size|
+    ['mid', 'thumb'].each do |size|
       0.upto(2) do |index|
         touch("#{dest_filespec}.#{size}.#{index}.jpg")
       end
     end
-    controller = FanartController.new(nil)
-    controller.link_fanart(dest_filespec)
+    @controller.send('link_fanart', dest_filespec)
     (File.exist?(link_filespec).should be_true) && (open(link_filespec).read.should == "#{dest_filespec}.mid.0.jpg\n")
-  end
-
-  it 'should link to the first, largest (cover) fanart file' do
-    dest_filespec = "#{TMPDIR}/foo-fanart"
-    link_filespec = "#{dest_filespec}.jpg"
-    ['cover', 'thumb'].each do |size|
-      0.upto(2) do |index|
-        touch("#{dest_filespec}.#{size}.#{index}.jpg")
-      end
-    end
-    controller = FanartController.new(nil)
-    controller.link_fanart(dest_filespec)
-    (File.exist?(link_filespec).should be_true) && (open(link_filespec).read.should == "#{dest_filespec}.cover.0.jpg\n")
   end
 
   it 'should link to the first, largest (thumb) fanart file' do
@@ -125,21 +112,37 @@ describe "FanartController" do
         touch("#{dest_filespec}.#{size}.#{index}.jpg")
       end
     end
-    controller = FanartController.new(nil)
-    controller.link_fanart(dest_filespec)
+    @controller.send('link_fanart', dest_filespec)
     (File.exist?(link_filespec).should be_true) && (open(link_filespec).read.should == "#{dest_filespec}.thumb.0.jpg\n")
   end
 
   it 'should fetch fanart' do
-    FileUtils.touch(File.join(TMPDIR, 'Die Hard - 1988.dummy'))
-    media = Media.new(TMPDIR, 'Die Hard - 1988.dummy')
-    media.imdb_id = 'tt0095016'
-    controller = FanartController.new(media)
-    controller.send('fetch_fanart', 'tt0095016')
+    @controller.send('fetch_fanart', 'tt0095016')
     buf = []
     %w(mid original thumb).each do |size|
       filespec = File.join(TMPDIR, "Die Hard - 1988-fanart.#{size}.0.jpg")
       buf << filespec unless (File.exist?(filespec).should be_true) && (File.size(filespec).should > 0)
+    end
+    puts buf.join("\n") unless buf.empty?
+    buf.empty?.should be_true
+  end
+
+  it 'should fetch fanart with unicode characters in the URL' do
+    buf = []
+    titles = {'While You Were Sleeping' => 'tt0114924',
+              'Sniper' => 'tt0108171',
+              'Leon The Professional' => 'tt0110413'
+             }
+    titles.each do |title, imdb_id|
+      FileUtils.touch(File.join(TMPDIR, "#{title}.dummy"))
+      media = Media.new(TMPDIR, "#{title}.dummy")
+      media.imdb_id = 'tt0095016'
+      controller = FanartController.new(media)
+      controller.send('fetch_fanart', imdb_id)
+      %w(mid original thumb).each do |size|
+        filespec = File.join(TMPDIR, "#{title}-fanart.#{size}.0.jpg")
+        buf << filespec unless (File.exist?(filespec).should be_true) && (File.size(filespec).should > 0)
+      end
     end
     puts buf.join("\n") unless buf.empty?
     buf.empty?.should be_true
