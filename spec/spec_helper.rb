e47958a759c02877ea8eeea5a8fde0a0aa57d5e8
Roy Wright <roy@wright.org>
2009-04-20 09:16:58 -0500

    prep for moving read_page_cache to its own gem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,8 +14,8 @@ require 'dvdprofiler2xbmc'
 TMPDIR = File.join(File.dirname(__FILE__), '../tmp')
 SAMPLES_DIR = File.join(File.dirname(__FILE__), 'samples')
 
-require 'cache_extensions'
-CacheExtensions.attach_to_read_page_classes(SAMPLES_DIR)
+require 'read_page_cache'
+ReadPageCache.attach_to_classes(SAMPLES_DIR)
 
 Signal.trap("INT") { exit(-1) }
 
